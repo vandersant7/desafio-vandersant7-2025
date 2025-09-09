5bb3274fa36062a66b7ee5769e6f3dddfae2c953
Evandro Santos <vandersant7@gmail.com>
2025-09-09 00:06:17 -0300

    feat: implementação encontraPessoas

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,7 +1,32 @@
+import { AdocaoService } from './adocao-service.js'
+
 class AbrigoAnimais {
+  constructor() {
+    this.service = new AdocaoService()
+  }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    try {
+      return this.service.processarAdocoes(
+        brinquedosPessoa1,
+        brinquedosPessoa2,
+        ordemAnimais
+      )
+    } catch (error) {
+      return { erro: error.message }
+    }
   }
 }
 
-export { AbrigoAnimais as AbrigoAnimais };
+export { AbrigoAnimais as AbrigoAnimais }
+
+const abrigo = new AbrigoAnimais()
+console.log(abrigo.encontraPessoas('RATO,BOLA', 'RATO,NOVELO', 'Rex,Fofo'))
+console.log(abrigo.encontraPessoas('CAIXA,RATO', 'RATO,BOLA', 'Lulu'))
+console.log(
+  abrigo.encontraPessoas(
+    'BOLA,LASER',
+    'BOLA,NOVELO,RATO,LASER',
+    'Mimi,Fofo,Rex,Bola'
+  )
+)
